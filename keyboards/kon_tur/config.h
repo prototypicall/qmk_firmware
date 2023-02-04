062cc84917179cc29c80d43dec215f1ea6e3b424
U-TABLETPC\erdem <aspart.ns@gmail.com>
2023-02-04 15:39:48 -0800

    First commit for kon_tur

---
+++ keyboards/kon_tur/config.h
@@ -0,0 +1,27 @@
+// Copyright 2023 prototypicall (@prototypicall)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// #define EE_HANDS
+#define MASTER_LEFT
+#define USE_I2C
+
+// Will eventually need to be set (for the 'styk')
+// #define SPLIT_LAYER_STATE_ENABLE
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
