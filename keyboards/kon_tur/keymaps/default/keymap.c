6d8c48239b898b60ca3a033c9d63ea7d02f27b32
U-TABLETPC\erdem <aspart.ns@gmail.com>
2023-02-06 23:07:47 -0800

    Fixed row and column issues. Tested with temp one layer keymap.

--- keyboards/kon_tur/keymaps/default/keymap.c
+++ keyboards/kon_tur/keymaps/default/keymap.c
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,                                        KC_H   ,KC_J   ,KC_K    ,KC_L   ,KC_SCLN,KC_QUOT,
         KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,                                        KC_N   ,KC_M   ,KC_COMM ,KC_DOT ,KC_SLSH,KC_RSFT,
                KC_GRV  ,KC_LWIN,KC_LBRC,KC_RBRC,                                                       KC_LEFT ,KC_UP   ,KC_DOWN,KC_RGHT,
-                                                        KC_LGUI,KC_LALT,        KC_RALT,KC_RCTL,
+                                                        DB_TOGG,KC_LALT,        KC_RALT,KC_RCTL,
                                                 KC_BSPC,KC_DEL ,KC_HOME,        KC_PGUP,KC_ENT ,KC_SPC,
                                                                 KC_END ,        KC_PGDN,
                                                 KC_CAPS,                                        KC_NUM,
